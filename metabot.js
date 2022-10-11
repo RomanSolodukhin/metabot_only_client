2726854c8a5b8605e13581ec02fe58774266e02c
RomanSolodukhin <35594467+RomanSolodukhin@users.noreply.github.com>
2022-10-11 05:36:42 +0300

    добавлена версия со ссылками и навигацией

--- metabot.js
+++ metabot.js
@@ -1,4 +1,5 @@
 var screennameRegex="[A-Za-z0-9_]+"
+let currentPostBots = window.localStorage.getItem('currentPostBots') || null
 
 /* отображение кол-ва ботов ==== начало */
 class injectionManager { // вот это бы заменить на какие-то хуки сервис воркера
@@ -10,28 +11,50 @@ class injectionManager { // вот это бы заменить на какие-
 	}
 	start () {
 		let interval = 500
-		if (this.timerId) clearTimeout(this.timerId)
+		if (this.timerId) clearInterval(this.timerId)
 		if (this.context) this.timerId = setInterval(this.callback.bind(this.context), interval)
 		else this.timerId = setInterval(this.callback, interval)
 	}
 	stop () {
-		clearTimeout(this.timerId)
+		clearInterval(this.timerId)
 	}
 }
 
 const bots = {
-	IDs: [],
-	get botsCount () {
-		return this.IDs.length
+	bots_posts: {
+		'https://twitter.com/dw_russian/status/1575888200534327297': {
+			bots: {
+				'GaziirHabbas': [
+					'https://twitter.com/GaziirHabbas/status/1575898174736121856',
+					'https://twitter.com/GaziirHabbas/status/1575898174736121856',
+					'https://twitter.com/GaziirHabbas/status/1575898174736121856'
+				],
+				'jhhIEGhZBSEoCQT': [
+					'https://twitter.com/jhhIEGhZBSEoCQT/status/1576105834580279296'
+				],
+				'W46cK5VZVbsXwCZ': [
+					'https://twitter.com/W46cK5VZVbsXwCZ/status/1576054093139054592'
+				]
+			}
+		},
+		'https://twitter.com/jhhIEGhZBSEoCQT/status/1576105834580279296': {
+			bots: {'fskdkfkd': [], 'sdfsdf': [], 'fsdfkfkmfmf': [], 'fsdfkkfmfmfmg': []}
+		},
+		'https://twitter.com/SpindelBerg/status/1575889921998721024': {
+			bots: {'fskdkfkd': [], 'sdfsdf': [], 'fsdfkfkmfmf': [], 'fsdfkkfmfmfmg': []}
+		}
 	},
-	posts: {},
-	showBotsList () {
+	currentPostBots: null,
+	lastLinkOpened: null,
+	showBotsList (postUrl) {
+		this.currentPostBots = postUrl
+		window.localStorage.setItem('currentPostBots', this.currentPostBots)
 		let modal = document.querySelector('[id="bots-list"]')
 		if (!modal) {
 			const iconStyles = 'width: 20px; height: 20px; fill: currentColor;'
-
-			document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').style.filter = 'blur(20px)'
-			document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').setAttribute('aria-hidden', true)
+			let rootElement = document.querySelector('[data-at-shortcutkeys]')
+			rootElement.style.filter = 'blur(20px)'
+			rootElement.setAttribute('aria-hidden', true)
 			document.querySelector('html').style.overflow = 'hidden'
 
 			modal = document.createElement('div')
@@ -42,6 +65,7 @@ const bots = {
 			document.querySelector('[id="layers"]').appendChild(modal)
 
 			let modalWindow = document.createElement('div')
+			modalWindow.setAttribute('id', 'bots-list-window')
 			modalWindow.style = `background-color: ${document.querySelector('body').style.backgroundColor}; display: flex; flex-direction: column; padding: 10px; height: auto; border-radius: 10px; max-height: 100vh; overflow-y: visible; width: max-content; max-width: 600px;`
 				+'font: 14px -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;'
 			modalWindow.classList.add('r-yfoy6g')
@@ -84,14 +108,24 @@ const bots = {
 			div {
 				-ms-overflow-style: none;  /* IE и Edge */
 				scrollbar-width: none;  /* Firefox */
-			}`
+			}
+			[collapsed] {
+				-moz-transform: translate(-50px, 0px);
+				-webkit-transform: translate(-50px, 0px);
+				-o-transform: translate(-50px, 0px);
+				-ms-transform: translate(-50px, 0px);
+				transform: translate(-50px, 0px);
+				transition: all 0.2s ease-out;
+			}
+			`
 			modalWindow.appendChild(styles)
 
 			let textColorClass = document.querySelector('body').style.backgroundColor === 'rgb(255, 255, 255)' ? 'r-18jsvk2' : 'r-vlxjld'
 
 			let modalHeader = document.createElement('div')
 			modalHeader.classList.add(textColorClass)
-			modalHeader.innerHTML = `<span style="flex-grow: 10; padding-left: 2em; padding-right: 2em;"><h2>Bots ${this.botsCount}. Replies ${Object.keys(this.posts).length}</h2></span>`
+			let botsCount = (this.bots_posts[postUrl]) && Object.prototype.hasOwnProperty.call(this.bots_posts[postUrl], 'bots') ? Object.keys(this.bots_posts[postUrl].bots).length : 0
+			modalHeader.innerHTML = `<span style="flex-grow: 10; padding-left: 2em; padding-right: 2em;"><h2>Bots ${botsCount}</h2></span>`
 			modalHeader.style = 'display: flex;'
 			modalWindow.appendChild(modalHeader)
 
@@ -104,7 +138,7 @@ const bots = {
 			closeButton.color = 'rgb(239, 243, 244)'
 
 			closeButton.addEventListener('click', () => {
-				this.hideBotsList()
+				this.closeBotsList()
 			})
 			modalHeader.appendChild(closeButton)
 
@@ -112,62 +146,100 @@ const bots = {
 			modalContent.classList.add('r-18jsvk2')
 			modalContent.classList.add('r-vlxjld')
 			modalContent.classList.add('posts')
-			if (Object.keys(this.posts).length > 0) {
-				Object.values(this.posts).map(HTMLElement => {
-					let post = document.createElement('article')
-					post.innerHTML = HTMLElement.querySelector('div > div > div > div.css-1dbjc4n.r-18u37iz > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu').innerHTML
-					post.querySelector('[role="group"]').remove()
-					post.style.paddingBottom = '2em'
-					modalContent.appendChild(post)
+			if ((this.bots_posts[postUrl]) && Object.prototype.hasOwnProperty.call(this.bots_posts[postUrl], 'bots') && Object.keys(this.bots_posts[postUrl].bots).length > 0) {
+				Object.keys(this.bots_posts[postUrl].bots).map(bot => {
+					let botPosts = document.createElement('div')
+					botPosts.innerHTML = `<span background: white; border-radius: 10px; color: black;>${bot}: </span>`
+					botPosts.style.paddingBottom = '2em'
+					this.bots_posts[postUrl].bots[bot].forEach(url => {
+						let post = document.createElement('article')
+						post.innerHTML = `<a ${url === window.location.href.replace(window.location.hash, '') ? 'style="background-color: rgba(255, 255, 255, 0.2);"' : ''}href="${url}#bots_window">${url.replace(/https:\/\/twitter.com\/.*\/status\//, '')}</a>`
+						botPosts.appendChild(post)
+					})
+					modalContent.appendChild(botPosts)
 				})
 			} else modalContent.innerHTML = '<span><h2>Ничего нет...</h2></span>'
 			modalWindow.appendChild(modalContent)
 		}
 	},
-	hideBotsList () {
+	closeBotsList () {
 		let modal = document.querySelector('[id="bots-list"]')
 		document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').style.filter = 'blur(0px)'
 		document.querySelector('html').style.overflow = 'auto scroll'
 		
 		document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').setAttribute('aria-hidden', false)
 		if (modal) modal.remove()
+		if (document.querySelector('[id="bots-list-window"]')) document.querySelector('[id="bots-list-window"]').remove()
 	},
-	injectBotsCounter () {
-		const botIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M320 0c17.7 0 32 14.3 32 32V96H480c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H160c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256c0-22.1-17.9-40-40-40s-40 17.9-40 40s17.9 40 40 40s40-17.9 40-40zm152 40c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40s17.9 40 40 40zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z"/></svg>'
-		let rootTweet = document.querySelector('article[tabindex="-1"] > div > div > div > div > div[role=group] > div') // 'article > div > div > div > div > div[role=group] > div'
-		if (rootTweet !== null) {
+	hideBotList () {
+		let modal = document.querySelector('[id="bots-list"]')
+		document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').style.filter = 'blur(0px)'
+		document.querySelector('html').style.overflow = 'auto scroll'
+		document.querySelector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010').setAttribute('aria-hidden', false)
+		let modalWindow = modal.querySelector('div')
+		document.querySelector('[role="navigation"]').appendChild(modalWindow)
+		setTimeout(() => modalWindow.scrollIntoView({ alignToTop: true }), 500)
+		modal.remove()
+	},
+	injectBotsCounterToTweet () {
+		const iconStyles = 'width: 20px; height: 20px; fill: currentColor;'
+		const botIcon = '<svg style="'+iconStyles+'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><g><path d="M320 0c17.7 0 32 14.3 32 32V96H480c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H160c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256c0-22.1-17.9-40-40-40s-40 17.9-40 40s17.9 40 40 40s40-17.9 40-40zm152 40c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40s17.9 40 40 40zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z"/></g></svg>'
+		document.querySelectorAll('article[tabindex="0"]').forEach(rootTweet => {
+			let navSection = rootTweet.querySelector('div[role=group]')
+			let botsCounter = rootTweet.querySelector('.bots-counter')
+			let replieLink = rootTweet.querySelectorAll('a[href]')[3].href
+			if (!botsCounter) {
+				botsCounter = document.createElement('div')
+				navSection.appendChild(botsCounter)
+				botsCounter.addEventListener('click', () => {
+					window.localStorage.setItem('bots_posts', JSON.stringify(this.bots_posts))
+					this.showBotsList(replieLink)
+				})
+			}
+			let botsCount = (this.bots_posts[replieLink]) && Object.prototype.hasOwnProperty.call(this.bots_posts[replieLink], 'bots') ? Object.keys(this.bots_posts[replieLink].bots).length : 0
+			botsCounter.innerHTML = `<div title="${replieLink}" class="bots-counter css-1dbjc4n r-18u37iz r-1h0z5md"><div aria-label="${botsCount} Bots" role="button" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr" data-testid="bots"><div dir="ltr" class="css-901oao r-1awozwy r-115tad6 r-6koalj r-37j5jr r-a023e6 r-16dba41 r-1h0z5md r-rjixqe r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0" style=""><div class="css-1dbjc4n r-xoduu5"><div class="css-1dbjc4n r-1niwhzg r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg"></div>
+			${botIcon}
+			</div><div class="css-1dbjc4n r-xoduu5 r-1udh08x"><span data-testid="app-text-transition-container" style="transform: translate3d(0px, 0px, 0px); transition-property: transform; transition-duration: 0.3s;"><span class="css-901oao css-16my406 r-poiln3 r-n6v787 r-1cwl3u0 r-1k6nrdp r-1e081e0 r-qvutc0"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">${botsCount}</span></span></span></div></div></div></div>`
+		})
+	},
+	injectBotsCounterToThread () {
+		let rootTweet = document.querySelector('article[tabindex="-1"]') // 'article > div > div > div > div > div[role=group] > div'
+		if (rootTweet) {
+			let navSection = rootTweet.querySelector('div[role=group] div')
 			let botsCounter = document.querySelector('[id="bots-section"]')
 			if (!botsCounter) {
-				rootTweet.childNodes[rootTweet.childNodes.length-1].classList.add('r-1mf7evn')
+				navSection.childNodes[navSection.childNodes.length-1].classList.add('r-1mf7evn')
 				botsCounter = document.createElement('div')
-				rootTweet.appendChild(botsCounter)
+				navSection.appendChild(botsCounter)
+				let replieLink = rootTweet.querySelectorAll('a[href]')[3].href
 				botsCounter.addEventListener('click', () => {
-					this.showBotsList()
+					window.localStorage.setItem('bots_posts', JSON.stringify(this.bots_posts))
+					this.showBotsList(replieLink)
 				})
-				botsCounter.innerHTML = '<div id="bots-section" class="css-1dbjc4n"><div class="css-1dbjc4n"><a dir="auto" role="link" class="css-4rbku5 css-18t94o4 css-901oao r-vlxjld r-1loqt21 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"><div class="css-1dbjc4n r-xoduu5 r-1udh08x"><span data-testid="app-text-transition-container" style="transform: translate3d(0px, 0px, 0px); transition-property: transform; transition-duration: 0.3s;"><span class="css-901oao css-16my406 r-poiln3 r-1b43r93 r-b88u0q r-1cwl3u0 r-bcqeeo r-qvutc0"><span id="bots-counter" class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">'+this.botsCount+'</span></span></span></div> <span class="css-901oao css-16my406 r-115tad6 r-poiln3 r-1b43r93 r-1cwl3u0 r-bcqeeo r-qvutc0"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">Bots</span></span></a></div></div>'
+				let botsCount = (this.bots_posts[replieLink]) && Object.prototype.hasOwnProperty.call(this.bots_posts[replieLink], 'bots') ? Object.keys(this.bots_posts[replieLink].bots).length : 0
+
+				botsCounter.innerHTML = '<div id="bots-section" class="css-1dbjc4n"><div class="css-1dbjc4n"><a dir="auto" role="link" class="css-4rbku5 css-18t94o4 css-901oao r-vlxjld r-1loqt21 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"><div class="css-1dbjc4n r-xoduu5 r-1udh08x"><span data-testid="app-text-transition-container" style="transform: translate3d(0px, 0px, 0px); transition-property: transform; transition-duration: 0.3s;"><span class="css-901oao css-16my406 r-poiln3 r-1b43r93 r-b88u0q r-1cwl3u0 r-bcqeeo r-qvutc0"><span id="bots-counter" class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">'+botsCount+'</span></span></span></div> <span class="css-901oao css-16my406 r-115tad6 r-poiln3 r-1b43r93 r-1cwl3u0 r-bcqeeo r-qvutc0"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">Bots</span></span></a></div></div>'
 			}
 		}
 	},
 	botsUpdated () {
 		let botsCounter = document.querySelector('[id="bots-counter"]')
-		if (botsCounter) botsCounter.innerText = this.botsCount
-	},
-	addBot (id) {
-		if (!this.IDs.includes(id)) {
-			this.IDs.push(id)
-			this.botsUpdated()
-		}
-	},
-	addPost (HTMLElement) {
-		if (this.IDs.includes(HTMLElement.querySelector('a').href)) {
-			let replieLinkEl = HTMLElement.querySelector('div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a[href]')
-			if (replieLinkEl) this.posts[replieLinkEl.href] = HTMLElement // div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > div.css-1dbjc4n.r-18u37iz.r-1q142lx > a
-		}
+		// if (botsCounter) botsCounter.innerText = this.botsCount
 	}
 }
 
 window.addEventListener('load', function () {
-	let botsCounter = new injectionManager(bots.injectBotsCounter, bots)
+	let injectBotsCounterToThread = new injectionManager(bots.injectBotsCounterToThread, bots)
+	let injectBotsCounterToTweet = new injectionManager(bots.injectBotsCounterToTweet, bots)
+	if (window.location.hash === '#bots_window') {
+		this.setTimeout(() => {
+			bots.bots_posts = JSON.parse(window.localStorage.getItem('bots_posts'))
+			bots.currentPostBots = window.localStorage.getItem('currentPostBots')
+			bots.lastLinkOpened = window.localStorage.getItem('lastLinkOpened')
+			bots.showBotsList(bots.currentPostBots)
+			bots.hideBotList()
+		}, 1000)
+	}
 })
 
 /* отображение кол-ва ботов ==== конец */
@@ -370,9 +442,6 @@ function markTweets()
 						fullname.prepend(botCaption)
 						
 						elementToHighlight = t.parentNode
-
-						bots.addBot(t.querySelector('a[href]').href)
-						bots.addPost(t)
 						//// "Highlight tweets only if they are not retweeted-by,
 						//// no matter who retweeted or who posted the original tweet."
 						////
